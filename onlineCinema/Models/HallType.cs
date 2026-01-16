3af87f34706c0dd4be0e95417e93c299f8ed9f3f
StanislavSizhuk <sizhukstanislav@gmail.com>
2026-01-16 14:16:21 +0200

    Add Hall and HallType models

--- onlineCinema/Models/HallType.cs
+++ onlineCinema/Models/HallType.cs
@@ -1,6 +1,18 @@
-﻿namespace onlineCinema.Models
+﻿using onlineCinema.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace onlineCinema.Models 
 {
-	public class HallType
-	{
-	}
+    public class HallType
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)] 
+        public string Name { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal CostMultiplier { get; set; }
+        public ICollection<Hall> Halls { get; set; } = new List<Hall>();
+    }
 }
