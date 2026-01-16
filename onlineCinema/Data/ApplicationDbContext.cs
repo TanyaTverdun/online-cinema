bc2d42283973485ddad1cff4d21c80e62bf51897
Tanya Tverdun <tanyatverdun@gmail.com>
2026-01-16 18:34:29 +0200

    Update DbContext with models

--- onlineCinema/Data/ApplicationDbContext.cs
+++ onlineCinema/Data/ApplicationDbContext.cs
@@ -10,7 +10,9 @@ namespace onlineCinema.Data
 			: base(options)
 		{
 		}
-
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
         public DbSet<Hall> Halls { get; set; }
         public DbSet<HallType> HallTypes { get; set; }
     }
