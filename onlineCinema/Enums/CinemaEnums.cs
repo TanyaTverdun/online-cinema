c3bba4a5283368459a7e1b44bb139caf58b2bcc5
Tanya Tverdun <tanyatverdun@gmail.com>
2026-01-19 19:35:33 +0200

    create enums for models

---
+++ onlineCinema/Enums/CinemaEnums.cs
@@ -0,0 +1,33 @@
+﻿namespace onlineCinema.Enums
+{
+    public enum MovieStatus : byte
+    {
+        ComingSoon = 1,
+        Released = 2,
+        Archived = 3
+    }
+
+    public enum AgeRating : byte
+    {
+        Age0 = 0,
+        Age6 = 6,
+        Age12 = 12,
+        Age16 = 16,
+        Age18 = 18
+    }
+
+    public enum SeatType : byte
+    {
+        Standard = 1,
+        VIP = 2,
+        Sofa = 3
+    }
+
+    public enum PaymentStatus : byte
+    {
+        Pending = 0,
+        Completed = 1,
+        Failed = 2,
+        Refunded = 3
+    }
+}
