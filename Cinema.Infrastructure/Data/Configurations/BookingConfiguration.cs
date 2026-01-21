cf9ae1bfefdea097a78b125b5976a16dc6e1c50e
Ivano4kaaaaaa <i.Melko20@dpl.ukr.education>
2026-01-21 16:58:26 +0200

    ad configuration Snack, SnackBooking, Payment and Booking

---
+++ Cinema.Infrastructure/Data/Configurations/BookingConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using onlineCinema.Domain.Entities;
+
+namespace onlineCinema.Infrastructure.Data.Configurations
+{
+    public class BookingConfiguration : IEntityTypeConfiguration<Booking>
+    {
+        public void Configure(EntityTypeBuilder<Booking> builder)
+        {
+            builder.HasKey(b => b.BookingId);
+
+            builder.Property(b => b.EmailAddress)
+                .IsRequired()
+                .HasMaxLength(256);
+
+            builder.Property(b => b.CreatedDateTime)
+                .HasColumnType("datetime");
+
+            builder.HasOne(b => b.Payment)
+                .WithMany()
+                .HasForeignKey(b => b.PaymentId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+
+            builder.HasOne(b => b.ApplicationUser)
+                .WithMany()
+                .HasForeignKey(b => b.ApplicationUserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ Cinema.Infrastructure/Data/Configurations/SnackBookingConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineCinema.Domain.Entities;
+
+namespace onlineCinema.Infrastructure.Data.Configurations
+{
+    public class SnackBookingConfiguration : IEntityTypeConfiguration<SnackBooking>
+    {
+        public void Configure(EntityTypeBuilder<SnackBooking> builder)
+        {
+            builder.HasKey(sb => new { sb.SnackId, sb.BookingId });
+
+            builder.HasOne(sb => sb.Snack)
+                .WithMany(s => s.SnackBookings)
+                .HasForeignKey(sb => sb.SnackId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne(sb => sb.Booking)
+                .WithMany(b => b.SnackBookings)
+                .HasForeignKey(sb => sb.BookingId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
