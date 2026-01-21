3779b875480f513f84ae183ed067b49170b1d373
Tanya Tverdun <tanyatverdun@gmail.com>
2026-01-21 17:16:22 +0200

    createTicket configuration

---
+++ Cinema.Infrastructure/Data/Configurations/TicketConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using onlineCinema.Domain.Entities;
+
+namespace onlineCinema.Infrastructure.Data.Configurations
+{
+    public class TicketConfiguration : IEntityTypeConfiguration<Ticket>
+    {
+        public void Configure(EntityTypeBuilder<Ticket> builder)
+        {
+            builder.HasKey(t => t.TicketId);
+            builder.Property(t => t.Price)
+                .HasColumnType("money");
+
+            builder.HasOne(t => t.Session)
+                .WithMany(s => s.Tickets)
+                .HasForeignKey(t => t.SessionId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(t => t.Seat)
+                .WithMany(s => s.Tickets)
+                .HasForeignKey(t => t.SeatId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(t => t.Booking)
+                .WithMany(b => b.Tickets)
+                .HasForeignKey(t => t.BookingId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
