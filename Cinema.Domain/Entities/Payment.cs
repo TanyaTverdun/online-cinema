fafafd46d268f4a140e04d6051cec6f0b77a86cf
Ivano4kaaaaaa <i.Melko20@dpl.ukr.education>
2026-01-21 14:41:58 +0200

    Add entities booking, payment, snack, snackbooking

--- Cinema.Domain/Entities/Payment.cs
+++ Cinema.Domain/Entities/Payment.cs
@@ -1,6 +1,13 @@
-﻿namespace onlineCinema.Domain.Entities
+﻿using onlineCinema.Domain.Entities;
+
+namespace OnlineCinema.Domain.Entities;
+
+public class Payment
 {
-    public class Payment
-    {
-    }
+    public int PaymentId { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime PaymentDate { get; set; }
+    public byte Status { get; set; }
+
+    public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
 }
--- Cinema.Domain/Entities/SnackBooking.cs
+++ Cinema.Domain/Entities/SnackBooking.cs
@@ -1,6 +1,12 @@
-﻿namespace onlineCinema.Domain.Entities
+﻿using onlineCinema.Domain.Entities;
+
+namespace OnlineCinema.Domain.Entities;
+
+public class SnackBooking
 {
-    public class SnackBooking
-    {
-    }
+    public int SnackId { get; set; }
+    public Snack Snack { get; set; } = null!;
+
+    public int BookingId { get; set; }
+    public Booking Booking { get; set; } = null!;
 }
