094c81336e1a789fa363f1cc191d0cb3b8bb4a8f
Maksym Kovalenko <qwerta357422@gmail.com>
2026-01-21 16:35:06 +0200

    Update HallConfiguration.cs

--- Cinema.Infrastructure/Data/Configurations/HallConfiguration.cs
+++ Cinema.Infrastructure/Data/Configurations/HallConfiguration.cs
@@ -10,25 +10,18 @@ namespace onlineCinema.Infrastructure.Data.Configurations
         {
             builder.HasKey(h => h.HallId);
 
-            builder.Property(h => h.HallId)
-                   .HasColumnName("hall_id");
-
             builder.Property(h => h.CinemaId)
-                   .HasColumnName("cinema_id")
                    .IsRequired();
 
             builder.Property(h => h.HallNumber)
-                   .HasColumnName("hall_num")
                    .HasColumnType("tinyint")
                    .IsRequired();
 
             builder.Property(h => h.RowCount)
-                   .HasColumnName("row_count")
                    .HasColumnType("tinyint")
                    .IsRequired();
 
             builder.Property(h => h.SeatInRowCount)
-                   .HasColumnName("seat_in_row_count")
                    .HasColumnType("tinyint")
                    .IsRequired();
 
