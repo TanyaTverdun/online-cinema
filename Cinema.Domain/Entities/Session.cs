ed2e3d29217d654ffa80e826c49092df5c06f898
Maksym Kovalenko <qwerta357422@gmail.com>
2026-01-21 15:08:39 +0200

    Update Session.cs

--- Cinema.Domain/Entities/Session.cs
+++ Cinema.Domain/Entities/Session.cs
@@ -1,33 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
+﻿namespace onlineCinema.Domain.Entities;
 
-namespace onlineCinema.Domain.Entities
+public class Session
 {
-	public class Session
-	{
-		public int Id { get; set; }
-		[Required(ErrorMessage = "Please indicate the start time of the session")]
-		[Display(Name = "Start time")]
-		public DateTime StartTime { get; set; }
-		[Required(ErrorMessage = "Please indicate the price of a standard ticket")]
-		[Column(TypeName = "decimal(18,2)")]
-		[Display(Name = "Ticket price")]
-		public decimal TicketPrice { get; set; }
+    public int SessionId { get; set; }
+    public int MovieId { get; set; }
+    public int HallId { get; set; }
 
-		// Зв'язок із залом
-		[Required(ErrorMessage = "Please specify the hall")]
-		[Display(Name = "Hall")]
-		public int HallId { get; set; }
-		[ForeignKey("HallId")]
-		public Hall? Hall { get; set; }
+    public DateTime ShowingDateTime { get; set; }
+    public decimal BasePrice { get; set; }
 
-		// Зв'язок з фільмом
-		[Required(ErrorMessage = "Please specify the movie")]
-		[Display(Name = "Movie")]
-		public int MovieId { get; set; }
-		[ForeignKey("MovieId")]
-		public Movie? Movie { get; set; }
-
-		public ICollection<Ticket>? Tickets { get; set; }
-	}
+    public Movie Movie { get; set; } = null!;
+    public Hall Hall { get; set; } = null!;
 }
