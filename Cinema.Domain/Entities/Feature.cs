e99dd712a59d3970603e8987d4a7cec624a4f2aa
Tanya Tverdun <tanyatverdun@gmail.com>
2026-01-21 13:28:00 +0200

    create Feature entity

--- Cinema.Domain/Entities/Feature.cs
+++ Cinema.Domain/Entities/Feature.cs
@@ -2,5 +2,9 @@
 {
     public class Feature
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public ICollection<MovieFeature> MovieFeatures { get; set; } = new List<MovieFeature>();
     }
 }
