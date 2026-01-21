5fd18ee045dbb52e638254d5c905a04f6975b5e4
Maksym Kovalenko <qwerta357422@gmail.com>
2026-01-21 16:36:03 +0200

    Update SeatConfiguration.cs

--- Cinema.Infrastructure/Data/Configurations/SeatConfiguration.cs
+++ Cinema.Infrastructure/Data/Configurations/SeatConfiguration.cs
@@ -10,30 +10,22 @@ namespace onlineCinema.Infrastructure.Data.Configurations
         {
             builder.HasKey(s => s.SeatId);
 
-            builder.Property(s => s.SeatId)
-                   .HasColumnName("seat_id");
-
             builder.Property(s => s.HallId)
-                   .HasColumnName("hall_id")
                    .IsRequired();
 
             builder.Property(s => s.RowNumber)
-                   .HasColumnName("row_number")
                    .HasColumnType("tinyint")
                    .IsRequired();
 
             builder.Property(s => s.SeatNumber)
-                   .HasColumnName("seat_number")
                    .HasColumnType("tinyint")
                    .IsRequired();
 
             builder.Property(s => s.Type)
-                   .HasColumnName("type")
                    .HasColumnType("tinyint")
                    .IsRequired();
 
             builder.Property(s => s.Coefficient)
-                   .HasColumnName("coefficient")
                    .HasColumnType("float")
                    .IsRequired();
 
@@ -41,7 +33,6 @@ namespace onlineCinema.Infrastructure.Data.Configurations
                    .WithMany(h => h.Seats)
                    .HasForeignKey(s => s.HallId);
 
-            // Рекомендовано для логічної цілісності
             builder.HasIndex(s => new { s.HallId, s.RowNumber, s.SeatNumber })
                    .IsUnique();
         }
