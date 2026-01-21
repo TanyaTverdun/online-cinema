121706e618886054aa8e3330f401b61b35273295
Tanya Tverdun <tanyatverdun@gmail.com>
2026-01-21 15:58:26 +0200

    create configuration for Feature

---
+++ Cinema.Infrastructure/Data/Configurations/FeatureConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using onlineCinema.Domain.Entities;
+
+namespace onlineCinema.Infrastructure.Data.Configurations
+{
+    public class FeatureConfiguration : IEntityTypeConfiguration<Feature>
+    {
+        public void Configure(EntityTypeBuilder<Feature> builder)
+        {
+            builder.HasKey(f => f.Id);
+
+            builder.Property(f => f.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(f => f.Description)
+                .HasMaxLength(500);
+        }
+    }
+}
