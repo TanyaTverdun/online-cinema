d936c9269264c07e609c7d204194f646bf86408e
Tanya Tverdun <tanyatverdun@gmail.com>
2026-01-22 13:36:26 +0200

    create MovieRepository

--- Cinema.Infrastructure/Repositories/MovieRepository.cs
+++ Cinema.Infrastructure/Repositories/MovieRepository.cs
@@ -3,11 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using onlineCinema.Application.Interfaces;
+using onlineCinema.Domain.Entities;
+using onlineCinema.Infrastructure.Data;
 
 namespace onlineCinema.Infrastructure.Repositories
 {
     public class MovieRepository : IMovieRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public MovieRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Movie>> GetAllAsync()
+        {
+            return await _context.Movies
+                .ToListAsync();
+        }
+
+        public async Task<Movie?> GetByIdAsync(int id)
+        {
+            return await _context.Movies
+                .Include(m => m.MovieGenres)
+                    .ThenInclude(mg => mg.Genre)
+                .Include(m => m.MovieCasts)
+                    .ThenInclude(mc => mc.CastMember)
+                .Include(m => m.MovieDirectors)
+                    .ThenInclude(md => md.Director)
+                .Include(m => m.MovieLanguages)
+                    .ThenInclude(ml => ml.Language)
+                .Include(m => m.MovieFeatures)
+                    .ThenInclude(mf => mf.Feature)
+                .FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public async Task AddAsync(Movie movie)
+        {
+            await _context.Movies.AddAsync(movie);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Movie movie)
+        {
+            _context.Movies.Update(movie);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie != null)
+            {
+                _context.Movies.Remove(movie);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }
