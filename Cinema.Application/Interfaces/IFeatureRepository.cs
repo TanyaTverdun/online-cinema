9e1ff09098e9af68e1132d81a84195af4e33ba7b
Tanya Tverdun <tanyatverdun@gmail.com>
2026-01-22 13:18:11 +0200

    create IFeatureRepository

--- Cinema.Application/Interfaces/IFeatureRepository.cs
+++ Cinema.Application/Interfaces/IFeatureRepository.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using onlineCinema.Domain.Entities;
 
 namespace onlineCinema.Application.Interfaces
 {
     public interface IFeatureRepository
     {
+        Task<IEnumerable<Feature>> GetAllAsync();
+        Task<Feature?> GetByIdAsync(int id);
+        Task AddAsync(Feature feature);
+        Task UpdateAsync(Feature feature);
+        Task DeleteAsync(int id);
     }
 }
