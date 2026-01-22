465526132ca1ed1298f9061fe470cc98718358dc
Tanya Tverdun <tanyatverdun@gmail.com>
2026-01-22 13:20:35 +0200

    create FeatureRepository

--- Cinema.Infrastructure/Repositories/FeatureRepository.cs
+++ Cinema.Infrastructure/Repositories/FeatureRepository.cs
@@ -3,11 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using onlineCinema.Application.Interfaces;
+using onlineCinema.Domain.Entities;
+using onlineCinema.Infrastructure.Data;
 
 namespace onlineCinema.Infrastructure.Repositories
 {
     public class FeatureRepository : IFeatureRepository
     {
+        private readonly ApplicationDbContext _context;
+
+        public FeatureRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Feature>> GetAllAsync()
+        {
+            return await _context.Features.ToListAsync();
+        }
+
+        public async Task<Feature?> GetByIdAsync(int id)
+        {
+            return await _context.Features.FindAsync(id);
+        }
+
+        public async Task AddAsync(Feature feature)
+        {
+            await _context.Features.AddAsync(feature);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Feature feature)
+        {
+            _context.Features.Update(feature);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var feature = await _context.Features.FindAsync(id);
+            if (feature != null)
+            {
+                _context.Features.Remove(feature);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }
