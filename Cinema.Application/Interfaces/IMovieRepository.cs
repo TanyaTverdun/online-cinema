807d71ffe27f8dfcb4542c8bded82a84f5afc67c
Tanya Tverdun <tanyatverdun@gmail.com>
2026-01-22 13:32:22 +0200

    create IMovieRepository

--- Cinema.Application/Interfaces/IMovieRepository.cs
+++ Cinema.Application/Interfaces/IMovieRepository.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using onlineCinema.Domain.Entities;
 
 namespace onlineCinema.Application.Interfaces
 {
     public interface IMovieRepository
     {
+        Task<IEnumerable<Movie>> GetAllAsync();
+        Task<Movie?> GetByIdAsync(int id);
+        Task AddAsync(Movie movie);
+        Task UpdateAsync(Movie movie);
+        Task DeleteAsync(int id);
     }
 }
