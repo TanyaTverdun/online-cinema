a08de1737ba9cda697de6d23212438d69301d746
Tanya Tverdun <tanyatverdun@gmail.com>
2026-01-25 14:59:23 +0200

    create dtos

---
+++ Cinema.Application/DTOs/SeatDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using onlineCinema.Domain.Enums;
+
+namespace onlineCinema.Application.DTOs
+{
+    public class SeatDto
+    {
+        public int SeatId { get; set; }
+        public int Row { get; set; }
+        public int Number { get; set; }
+        public SeatType Type { get; set; }
+        public decimal Price { get; set; }
+        public bool IsBooked { get; set; }
+    }
+}
