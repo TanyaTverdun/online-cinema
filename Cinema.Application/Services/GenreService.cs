0907b417a6979d4fa9bc633c65a8a953476e2626
Tanya Tverdun <tanyatverdun@gmail.com>
2026-01-31 21:43:01 +0200

    my chang

--- Cinema.Application/Services/GenreService.cs
+++ Cinema.Application/Services/GenreService.cs
@@ -10,49 +10,6 @@ using onlineCinema.Application.Services.Interfaces;
 using onlineCinema.Application.Interfaces;
 using onlineCinema.Domain.Entities;
 
-namespace onlineCinema.Application.Services;
-
-public class GenreService : IGenreService
-{
-    private readonly IUnitOfWork _unitOfWork;
-    private readonly GenreMapping _mapper;
-
-    public GenreService(IUnitOfWork unitOfWork, GenreMapping mapper)
-    {
-        _unitOfWork = unitOfWork;
-        _mapper = mapper;
-    }
-
-    public async Task<IEnumerable<GenreDto>> GetAllAsync()
-    {
-        var entities = await _unitOfWork.Genre.GetAllAsync();
-        return entities.Select(e => _mapper.MapToDto(e));
-    }
-
-    public async Task<GenreDto?> GetByIdAsync(int id)
-    {
-        var entity = await _unitOfWork.Genre.GetByIdAsync(id);
-        return entity != null ? _mapper.MapToDto(entity) : null;
-    }
-
-    public async Task CreateAsync(GenreDto dto)
-    {
-        var entity = _mapper.MapToEntity(dto);
-        await _unitOfWork.Genre.AddAsync(entity);
-        await _unitOfWork.SaveAsync();
-    }
-
-    public async Task UpdateAsync(GenreDto dto)
-    {
-        var entity = await _unitOfWork.Genre.GetByIdAsync(dto.GenreId);
-        if (entity != null)
-        {
-            _mapper.UpdateEntityFromDto(dto, entity);
-            _unitOfWork.Genre.Update(entity);
-            await _unitOfWork.SaveAsync();
-        }
-    }
-
 namespace onlineCinema.Application.Services
 {
     public class GenreService : IGenreService
